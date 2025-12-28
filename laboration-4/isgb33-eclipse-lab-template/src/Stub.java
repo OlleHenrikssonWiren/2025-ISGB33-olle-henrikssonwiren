85ed76e6c28a020229ed3c14eae4041b95b53144
Olle Henriksson Wiren <olle.henrikssonwiren@gmail.com>
2025-12-28 17:26:01 +0100

    lagt till metoder upp till get actor

--- laboration-4/isgb33-eclipse-lab-template/src/Stub.java
+++ laboration-4/isgb33-eclipse-lab-template/src/Stub.java
@@ -3,11 +3,16 @@ import static com.mongodb.client.model.Filters.in;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Properties;
+import java.util.Arrays;
 
 import com.mongodb.client.model.Collation;
 import com.mongodb.client.model.CollationStrength;
+import com.mongodb.client.model.Projections;
+import kotlin.collections.ArrayDeque;
 import org.bson.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,9 +24,13 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.AggregateIterable;
+
 
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+
+import javax.print.Doc;
 
 public class Stub {
 
@@ -33,7 +42,7 @@ public class Stub {
         initMongo();
         startServer();
     }
-
+    // connect till mongodb
     private static void initMongo() throws Exception {
         Properties prop = new Properties();
 
@@ -58,25 +67,27 @@ public class Stub {
 
         logger.info("Connected to MongoDB, using database '{}'", dbName);
     }
-
+    //starta server via javelin
     private static void startServer() {
         Javalin app = Javalin.create().start(4567);
 
-        app.get("/title/{name}", Stub::getByTitle);
-       
+        //Get methods
+        app.get("/title/{title}", Stub::getByTitle); // get title
+        app.get("/fullplot/{title}", Stub::getFullplot); // get fullplot
+        app.get("/cast/{title}", Stub::getCast);
+        app.get("/genre/{genre}", Stub::getByGenre);
+        app.get("/actor/{actor}", Stub::getByActor);
     }
 
     private static void getByTitle(Context ctx) {
-        String inputTitle = ctx.pathParam("name");
+        String inputTitle = ctx.pathParam("title");
         //String title = capitalizeFully(inputTitle.toLowerCase(Locale.ROOT));
         System.out.println(inputTitle);
-        Collation col = Collation.builder()
-                .locale("en_US")
-                .collationStrength(CollationStrength.SECONDARY)
-                .build();
+        Collation col = ignoreCapsCollation();
         Document doc = myCollection.find(eq("title", inputTitle)).collation(col).first();
 
-        if (doc != null) {
+        if (doc != null) { // om objektet inte är tomt
+            // ta bort keys
             doc.remove("_id");
             doc.remove("poser");
             doc.remove("cast");
@@ -91,7 +102,116 @@ public class Stub {
         }
     }
 
-  
+    private static void getFullplot(Context ctx) {
+        String input = ctx.pathParam("title");
+        Collation col = ignoreCapsCollation();
+        Document doc = myCollection.find(eq("title", input)).projection(Projections.include("fullplot", "title")).collation(col).first();
+
+        if (doc != null) {
+            doc.remove("_id");
+            ctx.status(200);
+            ctx.contentType("application/json");
+            ctx.result(doc.toJson());
+        }
+        else {
+            ctx.status(404);
+            ctx.contentType("application/json");
+            ctx.result(jsonError("Plot from movie not found.").toString());
+        }
+    }
+
+    private static void getCast(Context ctx) {
+        String input = ctx.pathParam("title");
+        Collation col = ignoreCapsCollation();
+        Document doc = myCollection.find(eq("title", input)).projection(Projections.include("title", "cast")).collation(col).first();
+
+        if (doc != null) {
+            doc.remove("_id");
+            ctx.status(200);
+            ctx.contentType("application/json");
+            ctx.result(doc.toJson());
+        }
+        else {
+            ctx.status(404);
+            ctx.contentType("application/json");
+            ctx.result(jsonError("Cast from movie not found.").toString());
+        }
+    }
+
+    private static void getByGenre(Context ctx) {
+        String input = ctx.pathParam("genre");
+        Collation col = ignoreCapsCollation();
+        //aggregation pipeline
+        AggregateIterable<Document> doc = myCollection.aggregate(
+                Arrays.asList(
+                        new Document("$match",
+                                new Document("genres", input)
+                        ),
+                        new Document("$limit", 10),
+                        new Document("$project",
+                                new Document("_id", 0)
+                                        .append("poster", 0)
+                                        .append("cast", 0)
+                                        .append("fullplot", 0)
+                        )
+                )
+        ).collation(col);
+        // lista för varje film
+        List<Document> list = new ArrayList<>();
+        doc.into(list); //stoppa in dokumenten i en lista
+        Document res = new Document("data", list); // skapa ett dokument med data från listan
+        System.out.println(doc);
+        if (doc.first() != null) { // om första dokumentet inte är null
+            ctx.status(200);
+            ctx.contentType("application/json");
+            ctx.result(res.toJson()); // dokumentet till json skickas som response
+        }
+        else {
+            ctx.status(404);
+            ctx.contentType("application/json");
+            ctx.result(jsonError("Movies from genre not found.").toString());
+        }
+    }
+
+    private static void getByActor(Context ctx) {
+        String input = ctx.pathParam("actor");
+        Collation col = ignoreCapsCollation();
+        AggregateIterable<Document> doc = myCollection.aggregate(
+                Arrays.asList(
+                        new Document("$match",
+                                new Document("cast", input)
+                        ),
+                        new Document("$limit", 10),
+                        new Document("$project",
+                                new Document("_id", 0)
+                                        .append("title", 1)
+                        )
+                )
+        ).collation(col);
+        List<Document> list = new ArrayList<>();
+        doc.into(list); //stoppa in dokumenten i en lista
+        Document res = new Document("data", list); // skapa ett dokument med data från listan
+
+        if (doc.first() != null) {
+            ctx.status(200);
+            ctx.contentType("application/json");
+            ctx.result(res.toJson()); // dokumentet till json skickas som response
+        }
+        else {
+            ctx.status(404);
+            ctx.contentType("application/json");
+            ctx.result(jsonError("Movie titles from actor not found.").toString());
+        }
+    }
+
+    // Metod för att skapa collation för att ignorera caps på mongodb med input.
+    private static Collation ignoreCapsCollation() {
+        return Collation.builder()
+                .locale("en_US")
+                .collationStrength(CollationStrength.SECONDARY)
+                .build();
+    }
+
     private static String capitalizeFully(String input) {
         String[] words = input.trim().split("\\s+");
 
